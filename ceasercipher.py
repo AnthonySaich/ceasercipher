f6648615ad61c927a92d07e878786cf58d5d9bfa
Anthony Saich <31777044+AnthonySaich@users.noreply.github.com>
2019-09-11 13:37:07 +0100

    Add files via upload

---
+++ ceasercipher.py
@@ -0,0 +1,19 @@
+L2I = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ",range(26)))
+I2L = dict(zip(range(26),"ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+
+
+
+key = 5
+plaintext = (input ("what message do you want encode "))
+
+# encrypt
+ciphertext = ""
+for c in plaintext.upper():
+    if c.isalpha(): ciphertext += I2L[ (L2I[c] + key)%26 ]
+    else: ciphertext += c
+
+
+
+print (plaintext)
+print (ciphertext)
+
